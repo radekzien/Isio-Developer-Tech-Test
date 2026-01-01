58f4e5f6e7ef4cada88152fc007d8fbdffa2ab43
radekzien <radoslaw.zien@outlook.com>
2026-01-01 12:19:15 +0000

    Created ConjuredItem class as subclass of RegularItem; Added comments

--- Python/item_classes.py
+++ Python/item_classes.py
@@ -7,18 +7,19 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
-class RegularItem(Item):
+class RegularItem(Item): #Regular items - degrade in quality by 1, degrade twice as fast after sell by date
     def __init__(self, name, sell_in, quality):
         super().__init__(name, sell_in, quality)
+        self.degrade_value = 1
 
     def update_quality(self):
         if self.quality > 0:
-            self.quality -= 1
+            self.quality -= self.degrade_value
         self.sell_in -= 1
         if self.sell_in < 0:
-            self.quality -=1
+            self.quality -= self.degrade_value
 
-class AgingItem(Item):
+class AgingItem(Item): #Aging item - increases in quality the older it gets up to 50
     def __init__(self, name, sell_in, quality):
         super().__init__(name, sell_in, quality)
     
@@ -27,14 +28,14 @@ class AgingItem(Item):
             self.quality += 1
         self.sell_in -= 1
 
-class LegendaryItem(Item):
+class LegendaryItem(Item): #Legendary Item - Never sold or decreases in quality
     def __init__(self, name, sell_in, quality):
         super().__init__(name, sell_in, quality)
     
     def update_quality(self):
         pass
 
-class BackstageItem(Item):
+class BackstageItem(Item): #Bacstage item - Quality increases by 2 10 days before concert, or by 3 5 days before concert, but worthless after the concert
     def __init__(self, name, sell_in, quality):
         super().__init__(name, sell_in, quality)
     
@@ -43,6 +44,11 @@ class BackstageItem(Item):
             self.quality += 2
         elif self.sell_in <= 5:
             self.quality += 3
-        if self.sell_in <= 0:
+        if self.sell_in < 0:
             self.quality = 0
         self.sell_in -= 1
+
+class ConjuredItem(RegularItem): #Conjured item - Subclass of Regular Item - "Conjured items degrade in Quality twice as fast as normal items"
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+        self.degrade_value *=2
