aca29688230360d1147017fdc6c4bed523588dd7
radekzien <radoslaw.zien@outlook.com>
2026-01-01 12:05:41 +0000

    Added other item subclasses; Fixed decrement typo in RegularItem subclass

--- Python/item_classes.py
+++ Python/item_classes.py
@@ -13,7 +13,36 @@ class RegularItem(Item):
 
     def update_quality(self):
         if self.quality > 0:
-            self.quality =- 1
-        self.sell_in =- 1
+            self.quality -= 1
+        self.sell_in -= 1
         if self.sell_in < 0:
-            self.quality =-1
+            self.quality -=1
+
+class AgingItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+    
+    def update_quality(self):
+        if self.quality < 50:
+            self.quality += 1
+        self.sell_in -= 1
+
+class LegendaryItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+    
+    def update_quality(self):
+        pass
+
+class BackstageItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+    
+    def update_quality(self):
+        if self.sell_in <= 10 and self.sell_in > 5:
+            self.quality += 2
+        elif self.sell_in <= 5:
+            self.quality += 3
+        if self.sell_in <= 0:
+            self.quality = 0
+        self.sell_in -= 1
